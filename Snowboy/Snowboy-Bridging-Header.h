26b32d2aebe25a38a238a7fe4900e4bdfa14d5ee
Mostafa Wael <mostafawael7@hotmail.com>
2020-05-01 00:23:07 +0200

    Initial Commit

---
+++ Snowboy/Snowboy-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+#ifndef Snowboy_Bridging_Header_h
+#define Snowboy_Bridging_Header_h
+
+#import "SnowboyWrapper.h"
+
+#endif /* Snowboy_Bridging_Header_h */
